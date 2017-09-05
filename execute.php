1aa809266375ca380d8534cf2a7aba18a6965fe7
tondogiro <31413671+tondogiro@users.noreply.github.com>
2017-09-05 07:48:13 +0200

    wq

--- execute.php
+++ execute.php
@@ -14,29 +14,31 @@ header("Content-Type: application/json");
 $updateId = $request->update_id;
 if ($request->callback_query != null) 
 {
-	$firstname = $request->callback_query->message->chat->first_name;
+	$first_name = $request->callback_query->message->chat->first_name;
 	$chatId = $request->callback_query->message->chat->id;
 	$text = $request->callback_query->data;
     $request->callback_query->message->entities[0]->offset = $updateId + 1;
 }
 else
 {
-	$firstname = $request->message->chat->first_name;
+    $first_name = $request->message->chat->first_name;
 	$chatId = $request->message->chat->id;
 	$text = $request->message->text;
     $request->message->entities[0]->offset = $updateId + 1;
 }
 
+echo json_encode($content);
+
 if(strcmp($text, "/start") === 0)
 {
 	$response = 
-		"Ciao *$firstname*, benvenuto!\n"
+		"Ciao *$first_name*, benvenuto!\n"
 		."\n"
 		."Ti ricordiamo che tutto quello che riguarda *Girada Storm* non ha nulla a che vedere con *Girada*. Lo scopo di questo gruppo e di questo bot "
 		."è quello di offrire gratuitamente un aiuto agli utenti per trovare nel minor tempo possibile i 3 amici necessari per ottenere il massimo sconto. "
 		."Non siamo quindi responsabili nè dell'ordine nè del prodotto acquistato, per i quali potrai contattare direttamente Girada.";
 
-	$parameters = array('chat_id' => $chatId, "text" => $response, "parse_mode" => "Markdown", "reply_markup" => Markups::getHomeMenu());
+	$parameters = ['chat_id' => $chatId, "text" => $response, "parse_mode" => "Markdown", "reply_markup" => Markups::getHomeMenu()];
 	$parameters["method"] = "sendMessage";
 	echo json_encode($parameters);
 }
@@ -55,22 +57,19 @@ elseif(strcmp($text, "/listaprezzo") === 0)
 }
 elseif(strcmp($text, "/ordina") === 0)
 {
-    /**
+
     $response =
         "Verrai guidato passo passo per metterti in lista.\n"
-        ."Ricorda che devi fare questi passaggi *prima* di effettuare l'ordine su Girada.";
+        ."Ricorda che devi fare questi passaggi *prima* di effettuare l'ordine su Girada.\n"
+        ."\n"
+        ."Adesso inserisci il *nome*, senza cognome:";
 
 	$parameters = ['chat_id' => $chatId, "text" => $response, "parse_mode" => "Markdown"];
 	$parameters["method"] = "sendMessage";
 	echo json_encode($parameters);
 
-	$first_step = "Inserisci il nome, senza cognome";
-    $parameters = ['chat_id' => $chatId, "text" => $first_step, "parse_mode" => "Markdown"];
-    $parameters["method"] = "sendMessage";
-    echo json_encode($parameters);
-*/
-    $parameters = ['chat_id' => $chatId, "action" => "typing"];
-    $parameters["method"] = "sendChatAction";
+    $action_parameters = ['chat_id' => $chatId, "action" => "typing"];
+    $action_parameters["method"] = "sendChatAction";
 }
 elseif(strcmp($text, "/home") === 0)
 {
