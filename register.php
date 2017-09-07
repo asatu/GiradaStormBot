0e4aed4c16d01a294b401a5b513302bd4cf44e8e
tondogiro <31413671+tondogiro@users.noreply.github.com>
2017-09-07 07:51:42 +0200

    ds

--- register.php
+++ register.php
@@ -10,5 +10,4 @@ $WEBHOOK_URL = 'https://giradastormbot.herokuapp.com/execute.php';
 $BOT_TOKEN = '430468745:AAFf2enKI48Xr2vHqnFcWz3eyUK0pA_Mk44';
 
 $telegram = new Api(TelegramConfig::BOT_TOKEN);
-$telegram->removeWebhook();
-//$telegram->setWebhook(['url' => $WEBHOOK_URL]);
+$telegram->setWebhook(['url' => $WEBHOOK_URL]);
