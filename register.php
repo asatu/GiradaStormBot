b6422e4eb44919ace34f94539bf53a2de74b8612
tondogiro <31413671+tondogiro@users.noreply.github.com>
2017-09-07 07:47:23 +0200

    sa

--- register.php
+++ register.php
@@ -10,4 +10,4 @@ $BOT_TOKEN = '430468745:AAFf2enKI48Xr2vHqnFcWz3eyUK0pA_Mk44';
 
 $telegram = new Api(TelegramConfig::BOT_TOKEN);
 $telegram->removeWebhook();
-$telegram->setWebhook(['url' => $WEBHOOK_URL]);
+//$telegram->setWebhook(['url' => $WEBHOOK_URL]);
