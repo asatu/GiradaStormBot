cf4c65afe3360f07bd7b59641c6dafa91bad8d26
tondogiro <31413671+tondogiro@users.noreply.github.com>
2017-09-07 11:59:48 +0200

    asda

--- register.php
+++ register.php
@@ -5,9 +5,5 @@ require_once('vendor/autoload.php');
 
 use Telegram\Bot\Api;
 
-// PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://giradastormbot.herokuapp.com/execute.php';
-$BOT_TOKEN = '430468745:AAFf2enKI48Xr2vHqnFcWz3eyUK0pA_Mk44';
-
 $telegram = new Api(TelegramConfig::BOT_TOKEN);
-$telegram->setWebhook(['url' => $WEBHOOK_URL]);
+$telegram->setWebhook(['url' => TelegramConfig::WEBHOOK_URL]);
